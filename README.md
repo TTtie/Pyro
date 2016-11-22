f781ea528310aab05bd66e40a7d0b3db033aa2c8
TTtie <tttiett@outlook.com>
2016-11-22 16:25:58 +0100

    aboutbot command changes and improving readme.md

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Pyro-Bot
-owo another bot from me<br>
-but welp, better than [TTtie Bot](https://github.com/TTtie/TTtie-Bot)
+owo another bot from me
+# Dependencies
+- [Node.js](https://nodejs.org)
+- Discordie; `npm install discordie`
+- FFmpeg; get it yourself, not providing binaries
+- colors; idk why i did it; `npm install colors`
+# but welp, better than [TTtie Bot](https://github.com/TTtie/TTtie-Bot)
+ye boi i added multifile system, if you wanna improve it, send a DM on discord(TTtie#4719) or make a pull request.
+Now bye and have fun with the bot ok?
--- assets/cmds/aboutbot.js
+++ assets/cmds/aboutbot.js
@@ -1,3 +1,3 @@
 module.exports = function(param,clientArg, args) {
-param.message.channel.sendMessage(`${param.message.author.nickMention}\n**Pyro Bot**\nMade by **TTtie#4719**\nJoin our help server if you need help: <https://tttie.ga/redir.php?dst=dskrd>\nInvite me: <https://discordapp.com/oauth2/authorize?client_id=242249568794836993&scope=bot&permissions=70306816>`)
+param.message.channel.sendMessage(`${param.message.author.nickMention}\n**Pyro Bot**\nMade by **TTtie#4719**\nJoin our help server if you need help: <https://tttie.ga/redir.php?dst=dskrd>\nInvite me: <https://discordapp.com/oauth2/authorize?client_id=242249568794836993&scope=bot&permissions=70306816>\nSource code: <https://github.com/TTtie/Pyro-Bot>`);
 }
