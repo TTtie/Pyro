5ec7844ede5fad680b9ac3365d00b9e4f64a9eba
TTtie <tttiett@outlook.com>
2020-04-12 14:49:25 +0200

    use a ratelimit bucket for dbots posting

--- dbots.js
+++ dbots.js
@@ -1,15 +1,21 @@
 "use strict";
 const { post } = require("chainfetch");
 const { dbotstoken } = require("./config.json");
+const Bucket = require("./node_modules/eris/lib/util/Bucket");
+const ratelimitBucket = new Bucket(1, 2000); // for safety, should be 20 per second
 module.exports = (guilds, shard, shards) => {
     if (!dbotstoken) return Promise.resolve();
-    return post("https://discord.bots.gg/api/v1/bots/242249568794836993/stats")
-        .set({
-            "Content-Type": "application/json",
-            "Authorization": dbotstoken
-        }).send({
-            shardId: shard,
-            shardCount: shards,
-            guildCount: guilds
-        }).toJSON();
+    return new Promise((rs, rj) => {
+        ratelimitBucket.queue(() => {
+            post("https://discord.bots.gg/api/v1/bots/242249568794836993/stats")
+                .set({
+                    "Content-Type": "application/json",
+                    "Authorization": dbotstoken
+                }).send({
+                    shardId: shard,
+                    shardCount: shards,
+                    guildCount: guilds
+                }).toJSON().then(rs, rj);
+        })
+    })
 };
